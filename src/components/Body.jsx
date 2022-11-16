016bee7f15b5c538b2332fed44bd706ed3c11c43
sunitadhikari <sunitaadhikari2001@gmail.com>
2022-11-16 22:50:10 +0545

    message

---
+++ src/components/Body.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Body = () => {
+  return (
+   <>
+    <img src='me.jpg'></img>
+   </>
+  )
+}
+
+export default Body
---
+++ src/components/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+  
+<div>
+  <footer>
+    <div className="containe">
+      <div className='sec aboutUs'>
+        <h2>About Us</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure debitis sapiente, excepturi consectetur aperiam, fugiat laboriosam eaque nobis unde porro et saepe laborum quam doloribus minima molestias voluptas blanditiis aliquam!</p>
+      </div>
+      <ul className='sc'>
+        <li><a href='#'><i class="fa fa-facebook-official" aria-hidden="true"></i></a></li>
+        <li><a href='#'><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+        <li><a href='#'><i class="fa fa-twitter" aria-hidden="true"></i></a></li>
+        <li><a href='#'><i class="fa fa-youtube-play" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+  );
+}
+export default Footer
